46fc408f782b5ff5356081b624027797b05f7890
MohdFaisalBidda <biddafaisal@gmail.com>
2024-03-30 05:01:44 +0530

    Initial commit with server for signed url endpt.

---
+++ server/index.js
@@ -0,0 +1,69 @@
+import express from "express";
+import dotenv from "dotenv";
+import AWS from "aws-sdk";
+import bodyParser from "body-parser";
+import multer from "multer";
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 3001;
+const upload = multer();
+
+AWS.config.update({
+  credentials: {
+    accessKeyId: process.env.ACCESSKEYID,
+    secretAccessKey: process.env.SECRETACCESSKEY,
+  },
+  region: process.env.REGION,
+});
+
+const s3 = new AWS.S3();
+
+app.get("/images", (req, res) => {
+  const params = {
+    Bucket: process.env.BUCKET,
+  };
+
+  s3.listObjects(params, (err, data) => {
+    if (err) {
+      console.log("Error listing objects from S3:", err);
+      res.status(500).send("Error fetching images!");
+    } else {
+      const images = data.Contents.map((object) => ({
+        key: object.Key,
+        imageUrl: `/${object.Key}`,
+      }));
+      res.status(200).send(images);
+    }
+  });
+});
+
+app.post("/signed-url", upload.single("image"), async (req, res) => {
+  const { originalname, buffer } = req.file;
+
+  const params = {
+    Bucket: process.env.BUCKET,
+    Key: originalname,
+    Body: buffer,
+    ACL: "private",
+  };
+
+  try {
+    await s3.upload(params).promise();
+    const signedUrl = s3.getSignedUrl("getObject", {
+      Bucket: process.env.BUCKET,
+      Key: originalname,
+      Expires: 300,
+    });
+    console.log("Signed URL", signedUrl);
+    res.status(200).send(signedUrl);
+  } catch (error) {
+    console.error("Error uploading image to S3:", error);
+    res.status(500).send("Error uploading image to S3");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`server running at port ${PORT}`);
+});
